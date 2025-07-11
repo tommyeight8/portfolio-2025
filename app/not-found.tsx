93abec90a7a93ccb3363a1a72868888b196ef8f4
tommy <tommyvong88@gmail.com>
2025-07-10 22:16:42 -0700

    added not found

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function NotFound() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">Page Not Found</h1>
+      <p className="mt-2 text-gray-500">This page could not be found.</p>
+    </div>
+  );
+}
